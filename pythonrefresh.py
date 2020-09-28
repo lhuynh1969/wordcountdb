2dd9c814a1262df18d7e3e276410cd8cf77591ff
Linh G. Huynh <lhuynh1969@gmail.com>
2020-09-28 16:16:52 -0400

    initial commit

---
+++ pythonrefresh.py
@@ -0,0 +1,10 @@
+# Variables ... no declaration of type
+age = int(input('How old are you? '))
+name = 'Linh'
+
+if age > 18:
+    print('{} is {} years old.'.format(name, age))
+else:
+    print('You are younger than 18')
+
+
